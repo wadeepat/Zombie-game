bfb1c697a5f2d2991edc3b13a710b914fd0e03d7
wadeepat <leaw.wadeepat@gmail.com>
2020-04-07 15:13:26 +0700

    liw 7/4/63

--- record.h
+++ record.h
@@ -3,51 +3,58 @@
 #include<vector>
 #include<string>
 #include<Windows.h>
-#include"item.h"
+#include<iomanip>
 using namespace std;
 
-void showscore(int score){
+void goxy(int x,int y){
+    COORD coord;
+    coord.X=x;
+    coord.Y=y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+}
+void yourscore(int score){
     system("cls");
-    cout<<"------------------------------------------------------------------------\n";
-    cout<<"|                                                                      |\n";
-    cout<<"|                                                                      |\n";
-    cout<<"|                                                                      |\n";
-    cout<<"|                                                                      |\n";
-    cout<<"|                                                                      |\n";
-    cout<<"|                                                                      |\n";
-    cout<<"|                                                                      |\n";
-    cout<<"|                                                                      |\n";
-    cout<<"|                                                                      |\n";
-    cout<<"|                                                                      |\n";
-    cout<<"|                                                                      |\n";
-    cout<<"|                                                                      |\n";
-    cout<<"|                                                                      |\n";
-    cout<<"|                                                                      |\n";
-    cout<<"|                                                                      |\n";
-    cout<<"------------------------------------------------------------------------";
-    gotoxy(26,7);cout<<" Your score is";Sleep(1000);cout<<".";Sleep(800);cout<<".";Sleep(800);cout<<".";
-    gotoxy(26,8);cout<<"------------------";
-    gotoxy(26,9);cout<<"|  Score: ";cout<<setw(5)<<setfill('0')<<score<<"  |";
-    gotoxy(26,10);cout<<setfill(' ')<<"------------------\n";
+    cout<<endl<<endl<<endl<<endl<<endl;
+    cout<<"        ------------------------------------------------------------------------\n";
+    cout<<"        |                                                                      |\n";
+    cout<<"        |                                                                      |\n";
+    cout<<"        |                                                                      |\n";
+    cout<<"        |                                                                      |\n";
+    cout<<"        |                                                                      |\n";
+    cout<<"        |                                                                      |\n";
+    cout<<"        |                                                                      |\n";
+    cout<<"        |                                                                      |\n";
+    cout<<"        |                                                                      |\n";
+    cout<<"        |                                                                      |\n";
+    cout<<"        |                                                                      |\n";
+    cout<<"        |                                                                      |\n";
+    cout<<"        |                                                                      |\n";
+    cout<<"        |                                                                      |\n";
+    cout<<"        |                                                                      |\n";
+    cout<<"        ------------------------------------------------------------------------";
+    goxy(26+8,7+5);cout<<" Your score is";Sleep(1000);cout<<".";Sleep(800);cout<<".";Sleep(800);cout<<".";Sleep(800);
+    goxy(26+8,8+5);cout<<"------------------";
+    goxy(26+8,9+5);cout<<"|  Score: ";cout<<setw(5)<<setfill('0')<<score<<"  |";
+    goxy(26+8,10+5);cout<<setfill(' ')<<"------------------\n";
     Sleep(4000);
 }
-void showrecord(){
+// void showrecord(){
     
-    ifstream readname("player.txt");
-    vector<string>v_name; vector<int>v_score; string text;
-    while(getline(readname,text)){
-        v_name.push_back(text);
-    }
-    readname.close();
-    ifstream readscore("score.txt");
-    while(getline(readscore,text)){
-        v_score.push_back(stoi(text.c_str()));
-    }
-    readscore.close();
+//     ifstream readname("player.txt");
+//     vector<string>v_name; vector<int>v_score; string text;
+//     while(getline(readname,text)){
+//         v_name.push_back(text);
+//     }
+//     readname.close();
+//     ifstream readscore("score.txt");
+//     while(getline(readscore,text)){
+//         v_score.push_back(stoi(text.c_str()));
+//     }
+//     readscore.close();
 
-    for(int i=0;i<10;i++){
-        for(int j=0;j<v_score.size();j++) 
-    }
+//     for(int i=0;i<10;i++){
+//         for(int j=0;j<v_score.size();j++){
 
-
-}
+//         }
+//     }
+// }
